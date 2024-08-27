8d7c30b003f44d7e6901c6abb4219612433ffffa
yinjg <yinjg1997@163.com>
2024-08-27 18:28:59 +0800

    fix: op code

--- src/method/click_captcha_solver.py
+++ src/method/click_captcha_solver.py
@@ -3,6 +3,8 @@ import os
 from src.utils import ver_onnx
 from src.utils import yolo_onnx
 from src.utils import utils
+from src.utils.ver_onnx import PreONNX
+from src.utils.yolo_onnx import YOLOV5_ONNX
 
 
 class ClickCaptchaSolver(object):
@@ -22,8 +24,8 @@ class ClickCaptchaSolver(object):
                 raise Exception("Error! 请在windows下的python3.6、3.8、3.10环境下使用")
             yolo_path = decryption(yolo_path)
             per_path = decryption(per_path)
-        self.yolo = yolo_onnx.YOLOV5_ONNX(yolo_path, classes=['target', 'title', 'char'], providers=['CPUExecutionProvider'])
-        self.pre = ver_onnx.PreONNX(per_path, providers=['CPUExecutionProvider'])
+        self.yolo = YOLOV5_ONNX(yolo_path, classes=['target', 'title', 'char'], providers=['CPUExecutionProvider'])
+        self.pre = PreONNX(per_path, providers=['CPUExecutionProvider'])
 
     def run(self, image_path):
         """
@@ -31,32 +33,34 @@ class ClickCaptchaSolver(object):
         :param img: 图片的路径、二进制数据或图片矩阵
         :return: list ---> [{'crop': [x1, y1, x2, y2], 'classes': ''}
         """
-        img = utils.open_image(image_path)
-        data = self.yolo.decect(image_path)
+        img = utils.open_image(image_path)  # 打开图像
+        data = self.yolo.decect(image_path)  # 使用 YOLO 检测图像中的目标
         # 需要选择的字
-        targets = [i.get("crop") for i in data if i.get("classes") == "target"]
-        chars = [i.get("crop") for i in data if i.get("classes") == "char"]
+        targets = [i.get("crop") for i in data if i.get("classes") == "target"]  # 获取目标区域的坐标
+        chars = [i.get("crop") for i in data if i.get("classes") == "char"]  # 获取字符区域的坐标
         # 根据坐标进行排序
-        chars.sort(key=lambda x: x[0])
-        chars = [img.crop(char) for char in chars]
+        chars.sort(key=lambda x: x[0])  # 按照字符的 x 坐标进行排序
+        print('chars: ', chars)
+        chars = [img.crop(char) for char in chars]  # 裁剪出字符区域的图像
         result = []
-        for m, img_char in enumerate(chars):
+        for m, img_char in enumerate(chars):  # 遍历每个字符图像
             if len(targets) == 0:
-                break
+                break  # 如果没有目标区域，退出循环
             elif len(targets) == 1:
-                slys_index = 0
+                slys_index = 0  # 如果只有一个目标区域，直接选择它
             else:
                 img_target_list = []
-                for n, target in enumerate(targets):
-                    img_target = img.crop(target)
-                    img_target_list.append(img_target)
-                slys = self.pre.reason_all(img_char, img_target_list)
-                slys_index = slys.index(max(slys))
-            result.append(targets[slys_index])
-            targets.pop(slys_index)
+                for n, target in enumerate(targets):  # 遍历每个目标区域
+                    img_target = img.crop(target)  # 裁剪出目标区域的图像
+                    img_target_list.append(img_target)  # 将目标区域图像添加到列表中
+                # slys 相似度
+                slys = self.pre.reason_all(img_char, img_target_list)  # 对字符图像和所有目标图像进行推理
+                slys_index = slys.index(max(slys))  # 找到推理结果中最大值的索引
+            result.append(targets[slys_index])  # 将对应的目标区域添加到结果中
+            targets.pop(slys_index)  # 从目标列表中移除已经匹配的目标区域
             if len(targets) == 0:
-                break
-        return result
+                break  # 如果没有剩余的目标区域，退出循环
+        return result  # 返回结果列表
 
 
 if __name__ == '__main__':
--- src/utils/ver_onnx.py
+++ src/utils/ver_onnx.py
@@ -1,208 +1,126 @@
 # !/usr/bin/env python
 # -*-coding:utf-8 -*-
 
-"""
-# File       : ver_onnx.py.py
-# Time       ：2023/3/29 14:20
-# Author     ：yujia
-# version    ：python 3.6
-# Description：
-"""
 from PIL import Image
 import onnxruntime
 import numpy as np
-import time
-import numbers
 from io import BytesIO
-
-import cv2
-import random
-
 
 np.set_printoptions(precision=4)
 
 
-def drow_img(image_path,result, save_image_path="res2.jpg"):
-    img = cv2.imread(image_path)
-
-    def plot_one_box(x, img, color=None, label=None, line_thickness=None):
-        # Plots one bounding box on image img
-        tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-        color = color or [random.randint(0, 255) for _ in range(3)]
-        c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-        cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-        if label:
-            tf = max(tl - 1, 1)  # font thickness
-            t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-            c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-            cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-            cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-        return img
-
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(result))]
-    for i, xyxy in enumerate(result):
-        label = i + 1
-        img = plot_one_box(xyxy, img, label=str(label), color=colors[i], line_thickness=1)
-    cv2.imwrite(save_image_path,img)
-
-
-def preprocess_input(x):
-    x /= 255.0
-    return x
-
-
-def crop(img, i, j, h, w):
-    """Crop the given PIL Image.
-
-    Args:
-        img (PIL Image): Image to be cropped.
-        i (int): i in (i,j) i.e coordinates of the upper left corner.
-        j (int): j in (i,j) i.e coordinates of the upper left corner.
-        h (int): Height of the cropped image.
-        w (int): Width of the cropped image.
-
-    Returns:
-        PIL Image: Cropped image.
-    """
-    return img.crop((j, i, j + w, i + h))
-
-
-def center_crop(img, output_size):
-    if isinstance(output_size, numbers.Number):
-        output_size = (int(output_size), int(output_size))
-    w, h = img.size
-    th, tw = output_size
-    i = int(round((h - th) / 2.))
-    j = int(round((w - tw) / 2.))
-    return crop(img, i, j, th, tw)
-
-
-def resize(img, size, interpolation=Image.BILINEAR):
-    r"""Resize the input PIL Image to the given size.
-
-    Args:
-        img (PIL Image): Image to be resized.
-        size (sequence or int): Desired output size. If size is a sequence like
-            (h, w), the output size will be matched to this. If size is an int,
-            the smaller edge of the image will be matched to this number maintaing
-            the aspect ratio. i.e, if height > width, then image will be rescaled to
-            :math:`\left(\text{size} \times \frac{\text{height}}{\text{width}}, \text{size}\right)`
-        interpolation (int, optional): Desired interpolation. Default is
-            ``PIL.Image.BILINEAR``
-
-    Returns:
-        PIL Image: Resized image.
-    """
-    if isinstance(size, int):
-        w, h = img.size
-        if (w <= h and w == size) or (h <= w and h == size):
-            return img
-        if w < h:
-            ow = size
-            oh = int(size * h / w)
-            return img.resize((ow, oh), interpolation)
-        else:
-            oh = size
-            ow = int(size * w / h)
-            return img.resize((ow, oh), interpolation)
-    else:
-        return img.resize(size[::-1], interpolation)
-
-
-def cvtColor(image):
-    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
-        return image
-    else:
-        image = image.convert('RGB')
-        return image
-
-
-def letterbox_image(image, size, letterbox_image):
-    w, h = size
-    iw, ih = image.size
-    if letterbox_image:
-        '''resize image with unchanged aspect ratio using padding'''
-        scale = min(w/iw, h/ih)
-        nw = int(iw*scale)
-        nh = int(ih*scale)
-
-        image = image.resize((nw,nh), Image.BICUBIC)
-        new_image = Image.new('RGB', size, (128,128,128))
-        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
-    else:
-        if h == w:
-            new_image = resize(image, h)
-        else:
-            new_image = resize(image, [h ,w])
-        new_image = center_crop(new_image, [h ,w])
-    return new_image
-
-
 class PreONNX(object):
     def __init__(self, path, providers=None):
+        # 初始化 ONNX 模型
         if not providers:
-            providers = ['CPUExecutionProvider']
+            providers = ['CPUExecutionProvider']  # 默认使用 CPU 执行提供者
         self.sess = onnxruntime.InferenceSession(path, providers=providers)
-        self.loadSize = 512
-        self.input_shape = [105, 105]
+        self.loadSize = 512  # 加载图像的默认大小
+        self.input_shape = [105, 105]  # 输入图像的形状
 
     def sigmoid(self, x):
+        # Sigmoid 激活函数
         return 1 / (1 + np.exp(-x))
 
-    def zhuanhuan(self, file):
+    def convert_to_image(self, file):
+        """
+        将输入文件转换为 PIL 图像对象
+        :param file: 可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :return: PIL 图像对象
+        """
         # 图片转换为矩阵
         if isinstance(file, np.ndarray):
-            img = Image.fromarray(file)
+            img = Image.fromarray(file)  # 如果输入是 ndarray，转换为 PIL 图像
         elif isinstance(file, bytes):
-            img = Image.open(BytesIO(file))
+            img = Image.open(BytesIO(file))  # 如果输入是字节流，转换为 PIL 图像
         elif isinstance(file, Image.Image):
-            img = file
+            img = file  # 如果输入已经是 PIL 图像，直接使用
         else:
-            img = Image.open(file)
+            img = Image.open(file)  # 其他情况，假设输入是文件路径，打开图像
         return img
 
-    def open_image(self, file, input_shape, nc=3):
-        out = self.zhuanhuan(file)
-        # 改变大小 并保证其不失真
-        out = out.convert('RGB')
+    def load_and_process_image(self, file, input_shape, nc=3):
+        """
+        打开并处理图像
+        :param file: 可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :param input_shape: 目标图像的尺寸 (height, width)
+        :param nc: 通道数，默认为 3（RGB），如果为 1 则转换为灰度图像
+        :return: 处理后的 PIL 图像对象
+        """
+        # 打开并处理图像
+        img = self.convert_to_image(file)
+        img = img.convert('RGB')  # 转换为 RGB 模式
         h, w = input_shape
-        out = out.resize((w, h), 1)
+        img = img.resize((w, h), Image.Resampling.LANCZOS)  # 调整图像大小，使用高质量的 LANCZOS 采样
         if nc == 1:
-            out = out.convert('L')
-        return out
+            img = img.convert('L')  # 如果需要单通道，转换为灰度图像
+        return img
 
     def set_img(self, lines):
-        image = self.open_image(lines, self.input_shape, 3)
-        image = np.array(image).astype(np.float32) / 255.0
-        photo = np.expand_dims(np.transpose(image, (2, 0, 1)), 0)
+        """
+        预处理图像，将其转换为模型输入所需的格式
+        :param lines: 输入图像，可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :return: 预处理后的图像，格式为 (1, 3, height, width)
+        """
+        # 预处理图像
+        image = self.load_and_process_image(lines, self.input_shape, nc=3)
+        image = np.array(image).astype(np.float32) / 255.0  # 归一化
+        photo = np.expand_dims(np.transpose(image, (2, 0, 1)), 0)  # 调整维度顺序并扩展维度
         return photo
 
     def reason(self, image_1, image_2):
+        """
+        对一对图像进行推理，返回模型的输出
+        :param image_1: 第一张输入图像，可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :param image_2: 第二张输入图像，可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :return: 模型的输出，经过 Sigmoid 激活函数处理后的值
+        """
+        # 预处理图像
         photo_1 = self.set_img(image_1)
         photo_2 = self.set_img(image_2)
+
+        # 运行模型
         out = self.sess.run(None, {"x1": photo_1, "x2": photo_2})
+
+        # 处理模型输出
         out = out[0]
         out = self.sigmoid(out)
         out = out[0][0]
+
         return out
 
     def reason_all(self, image_1, image_2_list):
+        """
+        对多对图像进行推理，返回模型的输出列表
+        :param image_1: 第一张输入图像，可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :param image_2_list: 第二张输入图像的列表，每个元素可以是 ndarray、字节流、PIL 图像对象或文件路径
+        :return: 模型的输出列表，经过 Sigmoid 激活函数处理后的值
+        """
+        # 预处理第一张图像
         photo_1 = self.set_img(image_1)
-        photo_2_all = None
-        photo_1_all = photo_1
+
+        # 初始化 photo_2_all 和 photo_1_all
+        photo_2_all = []
+        photo_1_all = []
+
+        # 预处理 image_2_list 中的每张图像，并将其与 photo_1 连接
         for image_2 in image_2_list:
             photo_2 = self.set_img(image_2)
-            if photo_2_all is None:
-                photo_2_all = photo_2
-            else:
-                photo_2_all = np.concatenate((photo_2_all, photo_2))
-                photo_1_all = np.concatenate((photo_1_all, photo_1))
+            photo_2_all.append(photo_2)
+            photo_1_all.append(photo_1)
 
+        # 将列表转换为 numpy 数组，并沿第一个维度连接
+        photo_2_all = np.concatenate(photo_2_all, axis=0)
+        photo_1_all = np.concatenate(photo_1_all, axis=0)
+
+        # 运行模型
         out = self.sess.run(None, {"x1": photo_1_all, "x2": photo_2_all})
+
+        # 处理模型输出
         out = out[0]
         out = self.sigmoid(out)
         out = out.tolist()
-        out = [i[0] for i in out]
+        out = [i[0] for i in out]  # 提取每个输出的第一个元素
 
         return out
 
